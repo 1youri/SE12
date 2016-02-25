e2bde5a4a4ef86dbcf9ea37ef837f99a63017d85
1youri <yourish@live.nl>
2016-02-25 12:57:11 +0100

    first upload

---
+++ usecasehelper/usecasehelper/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace usecasehelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbCreate = new System.Windows.Forms.RadioButton();
+            this.rbSelect = new System.Windows.Forms.RadioButton();
+            this.rbActor = new System.Windows.Forms.RadioButton();
+            this.rbUseCase = new System.Windows.Forms.RadioButton();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.pbDraw = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbLine);
+            this.groupBox1.Controls.Add(this.rbUseCase);
+            this.groupBox1.Controls.Add(this.rbActor);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elementen";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbSelect);
+            this.groupBox2.Controls.Add(this.rbCreate);
+            this.groupBox2.Location = new System.Drawing.Point(180, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(161, 123);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mode";
+            // 
+            // rbCreate
+            // 
+            this.rbCreate.AutoSize = true;
+            this.rbCreate.Location = new System.Drawing.Point(16, 32);
+            this.rbCreate.Name = "rbCreate";
+            this.rbCreate.Size = new System.Drawing.Size(71, 21);
+            this.rbCreate.TabIndex = 0;
+            this.rbCreate.TabStop = true;
+            this.rbCreate.Text = "Create";
+            this.rbCreate.UseVisualStyleBackColor = true;
+            // 
+            // rbSelect
+            // 
+            this.rbSelect.AutoSize = true;
+            this.rbSelect.Location = new System.Drawing.Point(16, 59);
+            this.rbSelect.Name = "rbSelect";
+            this.rbSelect.Size = new System.Drawing.Size(68, 21);
+            this.rbSelect.TabIndex = 1;
+            this.rbSelect.TabStop = true;
+            this.rbSelect.Text = "Select";
+            this.rbSelect.UseVisualStyleBackColor = true;
+            // 
+            // rbActor
+            // 
+            this.rbActor.AutoSize = true;
+            this.rbActor.Location = new System.Drawing.Point(8, 27);
+            this.rbActor.Name = "rbActor";
+            this.rbActor.Size = new System.Drawing.Size(62, 21);
+            this.rbActor.TabIndex = 0;
+            this.rbActor.TabStop = true;
+            this.rbActor.Text = "Actor";
+            this.rbActor.UseVisualStyleBackColor = true;
+            // 
+            // rbUseCase
+            // 
+            this.rbUseCase.AutoSize = true;
+            this.rbUseCase.Location = new System.Drawing.Point(8, 54);
+            this.rbUseCase.Name = "rbUseCase";
+            this.rbUseCase.Size = new System.Drawing.Size(86, 21);
+            this.rbUseCase.TabIndex = 1;
+            this.rbUseCase.TabStop = true;
+            this.rbUseCase.Text = "UseCase";
+            this.rbUseCase.UseVisualStyleBackColor = true;
+            // 
+            // rbLine
+            // 
+            this.rbLine.AutoSize = true;
+            this.rbLine.Location = new System.Drawing.Point(7, 81);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(56, 21);
+            this.rbLine.TabIndex = 2;
+            this.rbLine.TabStop = true;
+            this.rbLine.Text = "Line";
+            this.rbLine.UseVisualStyleBackColor = true;
+            // 
+            // pbDraw
+            // 
+            this.pbDraw.BackColor = System.Drawing.Color.White;
+            this.pbDraw.Location = new System.Drawing.Point(13, 143);
+            this.pbDraw.Name = "pbDraw";
+            this.pbDraw.Size = new System.Drawing.Size(813, 422);
+            this.pbDraw.TabIndex = 2;
+            this.pbDraw.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 577);
+            this.Controls.Add(this.pbDraw);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbLine;
+        private System.Windows.Forms.RadioButton rbUseCase;
+        private System.Windows.Forms.RadioButton rbActor;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbSelect;
+        private System.Windows.Forms.RadioButton rbCreate;
+        private System.Windows.Forms.PictureBox pbDraw;
+    }
+}
+
