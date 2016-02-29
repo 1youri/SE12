d81e1bfbe278bd3e18885918bc486c1dd48b7cb7
1youri <yourish@live.nl>
2016-02-29 13:24:38 +0100

    worked on it

---
+++ usecasehelper/usecasehelper/Actor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace usecasehelper
+{
+    public class Actor
+    {
+
+        public string name;
+        public int x;
+        public int y;
+        public bool selected;
+        public Rectangle rect;
+
+        public Actor(int index, string naam, int x, int y)
+        {
+            this.name = naam;
+            this.x = x;
+            this.y = y;
+
+            selected = false;
+            this.rect = new Rectangle(x - 30, y - 50, 60, 110);
+        }
+
+        public static Rectangle DrawActor(Graphics g,Pen pen, Brush brush, int x, int y,string name)
+        {
+            Rectangle rect = new Rectangle(x - 30, y - 50, 60, 110);
+            g.FillRectangle(Brushes.White, rect);
+            g.DrawEllipse(pen, x-15 , y - 50, 30, 30); //head
+            g.DrawLine(pen, x, y - 20, x, y + 21); //torso
+            g.DrawLine(pen, x - 30, y - 10, x + 30, y - 10); //arms
+            g.DrawLine(pen, x, y + 20, x - 20, y + 60); //leftleg
+            g.DrawLine(pen, x, y + 20, x + 20, y + 60);//rightleg
+
+            g.DrawString(name, new Font("Comic Sans MS", 14), brush, x - g.MeasureString(name, new Font("Comic Sans MS", 14)).Width / 2, y + 70);
+
+            return rect;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ usecasehelper/usecasehelper/ActorForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace usecasehelper
+{
+    partial class ActorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbActornaam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbActornaam
+            // 
+            this.tbActornaam.Location = new System.Drawing.Point(13, 31);
+            this.tbActornaam.Name = "tbActornaam";
+            this.tbActornaam.Size = new System.Drawing.Size(257, 22);
+            this.tbActornaam.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name Actor";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(194, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ActorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 99);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbActornaam);
+            this.Name = "ActorForm";
+            this.Text = "ActorForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ActorForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbActornaam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
