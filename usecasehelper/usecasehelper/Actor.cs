1d9f24641bf1c35d395dbacd1e55085d3e1459ef
1youri <yourish@live.nl>
2016-03-11 13:47:00 +0100

    update

--- usecasehelper/usecasehelper/Actor.cs
+++ usecasehelper/usecasehelper/Actor.cs
@@ -29,7 +29,7 @@ namespace usecasehelper
         public static Rectangle DrawActor(Graphics g,Pen pen, Brush brush, int x, int y,string name)
         {
             Rectangle rect = new Rectangle(x - 30, y - 50, 60, 150);
-            g.FillRectangle(Brushes.White, rect);
+            g.FillRectangle(Brushes.White, rect); //background
             g.DrawEllipse(pen, x-15 , y - 50, 30, 30); //head
             g.DrawLine(pen, x, y - 20, x, y + 21); //torso
             g.DrawLine(pen, x - 30, y - 10, x + 30, y - 10); //arms
